dc25ba42106f1bbeada0402eeb800285ca3eec29
Rodrigo Fentanes <rodrigo.fentanes@hotmail.com>
2025-04-15 12:19:47 -0300

    Finalizacao dos commits pois preciso usar dependências e não é possível com a estrutura atual. Quero mantes a estrutura atual para ter como exempo.

--- src/AppListTest.java
+++ src/AppListTest.java
@@ -5,8 +5,11 @@ import br.com.fentanes.nomeDoProjeto.pacoteGeralDeTestes.Title;
 
 public class AppListTest {
   public static void main(String[] args) {
-    // Não podemos dar um new na classe "List" pois a mesma é uma interface, veja abaixo...
-    // List<Title> listTitles = new List<Title>(); 
+    /* 
+      Não podemos dar um new na classe "List" pois a mesma é uma interface, veja abaixo...
+      List<Title> listTitles = new List<Title>();
+    */ 
+
     // Devemos fazer da seguinte forma...
     List<Title> listTitles = new LinkedList<Title>(); 
 
---
+++ src/br/com/fentanes/nomeDoProjeto/pacoteGeralDeTestes/ResponseSearchMovieTO.java
@@ -0,0 +1,54 @@
+package br.com.fentanes.nomeDoProjeto.pacoteGeralDeTestes;
+
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+public class ResponseSearchMovieTO {
+  public class Result{
+    public boolean adult;
+    public String backdrop_path;
+    public ArrayList<Integer> genre_ids;
+    public int id;
+    public String original_language;
+    public String original_title;
+    public String overview;
+    public double popularity;
+    public String poster_path;
+    public String release_date;
+    public String title;
+    public boolean video;
+    public double vote_average;
+    public int vote_count;
+  }
+
+  public class Root{
+    public int page;
+    public ArrayList<Result> results;
+    public int total_pages;
+    public int total_results;
+  }
+
+  public ResponseSearchMovieTO parse(String json) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    ResponseSearchMovieTO data = objectMapper.readValue(json, ResponseSearchMovieTO.class);
+    return (ResponseSearchMovieTO) data;
+
+    // OU
+
+    ResponseSearchMovieTO data = new ObjectMapper().readValue(json, ResponseSearchMovieTO.class);
+    return (ResponseSearchMovieTO) data;
+
+    // OU
+
+    Gson gson = new Gson(); // Or use new GsonBuilder().create();
+    ResponseSearchMovieTO data = gson.fromJson(json, ResponseSearchMovieTO.class); // deserializes json into "data"
+    
+    // OU
+
+    ResponseSearchMovieTO data = new Gson().fromJson(json, ResponseSearchMovieTO.class);
+    return (ResponseSearchMovieTO) data;
+  }
+}
